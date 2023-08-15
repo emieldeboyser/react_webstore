b135bbb34bc5f5f31418b8e61d7ef4602f012826
Emielex <emiel.deboyser@student.arteveldehs.be>
2023-08-15 17:26:31 +0200

    DONE

--- app/app/src/Pages/App.js
+++ app/app/src/Pages/App.js
@@ -38,7 +38,6 @@ const App = () => {
           <Route path="/profile/edit" element={<EditProfile />} />
           <Route path="/wishlist" element={<WishlistPage />} />
           {/* admin routs */}
-          {/* only these routes are for the user role administrator */}
           <Route path="/admin" element={<Admin />} />
           <Route path="/admin/addCoupon" element={<AddCouponCode />} />
           <Route path="/admin/editCoupon/:id" element={<EditCoupon />} />
